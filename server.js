3dac2f9f0dea890e17f5ab92e369c82703a9ac3b
Vickii <200028pq@yse-c.net>
2021-12-08 12:06:06 +0900

    upload

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+//routes.js をよみこむ
+const routes = require('./routes')
+
 const dotenv = require('dotenv')
 
 dotenv.config()
@@ -7,31 +10,19 @@ const port = process.env.PORT
 
 const app = express()
 
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
 
-app.post('/auth',(req,res) => {
-    const login_name = req.body.login_name
-    const password = req.body.password
+//レイアウトをつかう
+const layouts = require('express-ejs-layouts')
+app.set('layout', 'layouts/default')
+app.use(layouts)
 
-    let message = 'ログインできません'
-    if(login_name == process.env.login_name
-        && password == process.env.PASSWORD){
-            message = 'ログインしました'
-        }
-    res.send(message)
-})
+//ejs をつかう
+app.set('view engine', 'ejs')
 
-// app.get(URL, 処理)
-app.get('/', (req, res) => {
-    console.log(req.query)
-
-    res.send('Hello YSE!!!!!')
-})
-
-app.get('/profile', (req, res) => {
-    res.send('This is Profile Page!!!!')
-})
+//routes をつかう
+app.use(routes)
 
 app.listen(port, host, () => {
     console.log('http://' + host + ':' + port)
